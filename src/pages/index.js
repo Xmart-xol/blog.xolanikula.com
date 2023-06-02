898d7f67886b019bcfe1638fb52b4edf55540a1f
Xolani Kula <xolani.kula@investec.co.uk>
2023-06-02 04:45:31 +0200

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Resume from './resume.js';
+import Resume from './resumemd.md';
 
 import styles from './index.module.css';
 
@@ -18,7 +18,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/resume.js">
+            to="/resumemd">
             My Resume
           </Link>
         </div>
---
+++ src/pages/resumemd.md
@@ -0,0 +1,3 @@
+# My Resume
+
+This is a resume page
